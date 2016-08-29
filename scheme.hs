738b9b9c73e667522574d7e4f23b5ca69f8e8538
Schroedingberg <aaronrebmann@gmail.com>
2016-08-29 18:35:32 +0200

    Add whitespace ignoring parser

--- scheme.hs
+++ scheme.hs
@@ -5,7 +5,7 @@ symbol :: Parser Char
 symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
 
 readExpr :: String -> String
-readExpr input = case parse symbol "lisp" input of
+readExpr input = case parse (spaces >> symbol) "lisp" input of
   Left err -> "No match: " ++ show err
   Right val -> "Found value"
 
@@ -14,3 +14,5 @@ main = do
   (expr:_) <- getArgs
   putStrLn(readExpr expr)
 
+spaces :: Parser ()
+spaces = skipMany1 space
